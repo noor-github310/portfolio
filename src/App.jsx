c7c02b37d5048d694c964af410b3ca6bfcef4493
noor-github310 <2200032542@kluniversity.in>
2025-02-05 10:18:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Service from './components/Service';
